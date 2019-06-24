73c45ffae4561f8b05ea0fef4427078bd197347c
panin717 <anatolypanin717@gmail.com>
2019-06-24 13:38:33 +0300

    Login page update

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -43,7 +43,7 @@ class Login extends Component {
       <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="8">
+            <Col md="5">
               <CardGroup>
                 <Card className="p-4">
                   <CardBody>
@@ -79,18 +79,6 @@ class Login extends Component {
                         <Button color="link" className="px-0">Forgot password?</Button>
                       </Col>
                     </Row>
-                  </CardBody>
-                </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Link to="/register">
-                        <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
-                      </Link>
-                    </div>
                   </CardBody>
                 </Card>
               </CardGroup>
